1d9d5fb622a1b82f08100de6cf2323c8ce2ebab4
kneeraj825 <kneeraj825@gmail.com>
2020-02-14 10:18:29 +0530

    Added:Largest and Second Largest Random Number

---
+++ shell/arrays/LargestSecondLargest.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -x
+echo "ten three digit random number and find the largest and smallest number in the array"
+	for (( count=0; count<10; count++))
+	do
+		a[count]=$((99+$RANDOM%900))
+	done
+		echo ${a[@]}
+		echo "length of the array is:" ${#a[@]}
+		largest=$(( a[0] ))
+		secondLargest=$(( a[0] ))
+		for(( index=0; index<=10; index++))
+		do
+			if (( $((a[index])) > $largest ))
+			then
+				secondLargest=$largest
+				largest=$(( a[index] ))
+			elif (( $(( a[index] )) >$secondLargest && $(( a[count] )) < $largest ))  
+			then
+				secondLargest=$(( a[index]  ))
+			fi
+		done
+echo "second largest number :" $largest
+echo "second largest number :" $secondLargest
